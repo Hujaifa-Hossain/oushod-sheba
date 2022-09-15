26f8d6e3f94b15cc710cc2236177ec9d8b77f271
Hujaifa21 <21hujaifa@gmail.com>
2022-09-16 01:23:12 +0600

    header added

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,24 @@
 import Image from "next/image";
 import React from "react";
-import logo from "../public/assets/health.jpg";
+import logo from "../public/assets/logo.png";
+import styles from '../styles/header.module.css';
 
 const Header = () => {
   return (
-    <div style={{display: 'flex', justifyContent: 'space-between'}}>
-      <Image
-        // loader={myLoader}
-        src={logo}
-        alt="Picture of the author"
-        width={100}
-        height={30}
-      />
+    <div className={styles.header} style={{ display: "flex", justifyContent: "space-between", height: '50px', alignItems: 'center'}}>
+      <div width={50} height={50}>
+        <Image
+          src={logo}
+          alt="Picture of the author"
+          width={50}
+        height={50}
+        />
+      </div>
 
-      <h2>Oushod Sheba</h2>
+      <h4>Oushod Sheba</h4>
+      <input type="text" placeholder="search anything" />
+      <li>help</li>
+      <li>Hujaifa Hossain</li>
     </div>
   );
 };
